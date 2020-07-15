3467d34d43c3310f51178ab5af253f5a506813c0
ali hosseini <sayed.ali.hosseini.1988@gmail.com>
2020-07-15 15:18:28 +0430

    Create : Slider Asset Ref
    the base slider for  life bar and time and etc ...

---
+++ sliderScript.cs
@@ -0,0 +1,37 @@
+﻿using RTLTMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class sliderScript : MonoBehaviour
+{
+    [SerializeField] private Image filledArea;
+
+    [SerializeField] private RTLTextMeshPro sliderText;
+//    [SerializeField] private int remains, value;
+//
+//    private void Update()
+//    {
+//        SetSliderValues(remains,value);
+//    }
+    public void SetSliderValues(float remains, float value)
+    {
+        var precent = remains / 100;
+        var valueToPercent = value / precent;
+        if (valueToPercent >= 100)
+        {
+            filledArea.fillAmount = 1;
+            sliderText.text = $"{remains}/{remains}";
+            return;
+        }
+
+        if (value <= 0)
+        {
+            sliderText.text =
+                sliderText.text = $"0/{remains}";
+            return;
+        }
+        // ReSharper disable once PossibleLossOfFraction
+        filledArea.fillAmount = value / 100;
+        sliderText.text = $"{value}/{remains}";
+    }
+}
